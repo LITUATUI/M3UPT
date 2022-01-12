aeaec56a1583bff871c84028eef0a5a0d99638bd
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-01-12 03:57:34 +0000

    Update streams.sh

--- scripts/streams.sh
+++ scripts/streams.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# TVI - delete the old stream URL, retrieve the new stream URL, put it in a text file then insert it in the m3u list
+# Remove old temporary TVI URLs
 
-sed -i '/live_tvi/d' m3upt.m3u
+sed -i '/wmsAuthSign/d' m3upt.m3u
+
+# TVI - retrieve the new stream URL, put it in a text file then insert it in the m3u list
 
 echo "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > TVI.txt
 
 sed -i '/"TVI"/r TVI.txt' m3upt.m3u
 
-# CNN Portugal - delete the old stream URL, retrieve the new stream URL, put it in a text file then insert it in the m3u list
-
-sed -i '/live_cnn/d' m3upt.m3u
+# CNN Portugal - retrieve the new stream URL, put it in a text file then insert it in the m3u list
 
 echo "https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > CNN_Portugal.txt
 
 sed -i '/"CNN-PT"/r CNN_Portugal.txt' m3upt.m3u
 
-# TVI Internacional - delete the old stream URL, retrieve the new stream URL, put it in a text file then insert it in the m3u list
-
-sed -i '/live_tvi_internacional/d' m3upt.m3u
+# TVI Internacional - retrieve the new stream URL, put it in a text file then insert it in the m3u list
 
 echo "https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > TVI_Internacional.txt
 
