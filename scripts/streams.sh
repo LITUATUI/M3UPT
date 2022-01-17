ca05cc800599a5bd5b555f8ab03b2dce34d7ef22
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-01-17 06:20:47 +0000

    Code optimization

--- scripts/streams.sh
+++ scripts/streams.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-# Remove old temporary TVI URLs
+# TVI - update the stream URL of TVI
 
-sed -i '/wmsAuthSign/d' m3upt.m3u
+sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
-# TVI - retrieve the new stream URL, put it in a text file then insert it in the m3u list
+# CNN Portugal - update the stream URL of CNN Portugal
 
-echo "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > TVI.txt
+sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
-sed -i '/"TVI"/r TVI.txt' m3upt.m3u
+# TVI Internacional - update the stream URL of TVI Internacional
 
-# CNN Portugal - retrieve the new stream URL, put it in a text file then insert it in the m3u list
-
-echo "https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > CNN_Portugal.txt
-
-sed -i '/"CNN-PT"/r CNN_Portugal.txt' m3upt.m3u
-
-# TVI Internacional - retrieve the new stream URL, put it in a text file then insert it in the m3u list
-
-echo "https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)" > TVI_Internacional.txt
-
-sed -i '/"TVI-INT"/r TVI_Internacional.txt' m3upt.m3u
+sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
 exit 0
