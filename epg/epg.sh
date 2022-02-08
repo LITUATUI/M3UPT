dff22844f0332feadf51e70af135d7aeeca2b4f2
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-02-08 01:23:10 +0000

    Create epg.sh

---
+++ epg/epg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/XMLTV/xmltv/raw/master/grab/pt_meo/tv_grab_pt_meo
+
+perl tv_grab_pt_meo --config-file meo.conf --output epg.xml
+
+gzip -c epg.xml > epg.xml.gz
+
+exit 0
