9be6f99538d78b955a967f06c533e2ae71107d2e
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-02-11 00:10:10 +0000

    Update epg_iptv-org.sh

--- epg/epg_iptv-org.sh
+++ epg/epg_iptv-org.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N https://iptv-org.github.io/epg/guides/pt/meo.pt.epg.xml && gzip -c meo.pt.epg.xml > meo.pt.epg.xml.gz
+wget -N https://iptv-org.github.io/epg/guides/pt/meo.pt.epg.xml && gzip -c meo-epg.xml > meo-epg.xml.gz
 
 exit 0
