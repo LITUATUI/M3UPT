6efcc2163d862d1d8d7a24c434708c60881e25a0
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-02-11 17:30:30 +0000

    Update epg_xmltv.sh

--- epg/epg_xmltv.sh
+++ epg/epg_xmltv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_meo/tv_grab_pt_meo && perl tv_grab_pt_meo --config-file meo.conf --days 1 --output meo-epg.xml && gzip -c meo-epg.xml > meo-epg.xml.gz
+wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_meo/tv_grab_pt_meo && perl tv_grab_pt_meo --config-file meo.conf --days 2 --output meo-epg.xml && gzip -c meo-epg.xml > meo-epg.xml.gz
 
 exit 0
