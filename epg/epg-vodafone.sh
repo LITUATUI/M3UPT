9cb53816048a99a3c3e48acb4aa4acfe5eb79af0
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-02-13 05:38:39 +0000

    Update epg-vodafone.sh

--- epg/epg-vodafone.sh
+++ epg/epg-vodafone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone && perl tv_grab_pt_vodafone --config-file vodafone.conf --output epg-vodafone.xml && sed -i '/.ottimg./d' epg-vodafone.xml && gzip -c vodafone-epg.xml > epg-vodafone.xml.gz
+wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone && perl tv_grab_pt_vodafone --config-file vodafone.conf --output epg-vodafone.xml && gzip -c vodafone-epg.xml > epg-vodafone.xml.gz
 
 exit 0
