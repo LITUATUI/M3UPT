f8a63c3a32f22016cc31f79645ea66c9aac6a688
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2022-12-11 02:07:42 +0000

    Add TVI Reality

--- streams.sh
+++ streams.sh
@@ -12,4 +12,8 @@ sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?
 
 sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
+# TVI Reality - update the stream URL of TVI Reality
+
+sed -i "/live_tvi_direct/ c https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
+
 exit 0
