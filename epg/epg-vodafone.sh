c8b400d13b6e18a63310c2bea3b0b43a308aeede
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2023-03-12 18:38:09 +0000

    Updates

--- epg/epg-vodafone.sh
+++ epg/epg-vodafone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone && perl tv_grab_pt_vodafone --config-file vodafone.conf --output epg-vodafone.xml && gzip -c epg-vodafone.xml > epg-vodafone.xml.gz
+wget -N https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone && perl tv_grab_pt_vodafone --config-file vodafone.conf --output epg-vodafone-pt.xml && gzip -c epg-vodafone-pt.xml > epg-vodafone-pt.xml.gz
 
 exit 0
