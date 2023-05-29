fd0935282e35d6f48160713457f84cad03761a5e
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2023-05-29 02:21:24 +0100

    Updates

--- EPG/epg-rtp.sh
+++ EPG/epg-rtp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd EPG && npm install && SITE=rtp.pt npm run grab && mv guides/pt/rtp.pt.xml epg-rtp.xml && xz -k -f -9 epg-rtp.xml && gzip -v9 -c epg-rtp.xml > epg-rtp.xml.gz
+cd iptv-org-epg && npm install && SITE=rtp.pt npm run grab && cd ~/EPG && mv iptv-org-epg/guides/pt/rtp.pt.xml epg-rtp.xml && xz -k -f -9 epg-rtp.xml && gzip -v9 -c epg-rtp.xml > epg-rtp.xml.gz
 
 exit 0
