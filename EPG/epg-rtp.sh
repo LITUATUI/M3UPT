92cfb23344df6bd33c9a8efa1b0958bf4f2c938d
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2023-05-29 03:00:05 +0100

    Update epg-rtp.sh

--- EPG/epg-rtp.sh
+++ EPG/epg-rtp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd iptv-org-epg && npm install && SITE=rtp.pt npm run grab && cd .. && mv iptv-org-epg/guides/pt/rtp.pt.xml /EPG/epg-rtp.xml && cd EPG && xz -k -f -9 epg-rtp.xml && gzip -v9 -c epg-rtp.xml > epg-rtp.xml.gz
+cd iptv-org-epg && npm install && SITE=rtp.pt npm run grab && mv iptv-org-epg/guides/pt/rtp.pt.xml ../EPG/epg-rtp.xml && cd EPG && xz -k -f -9 epg-rtp.xml && gzip -v9 -c epg-rtp.xml > epg-rtp.xml.gz
 
 exit 0
