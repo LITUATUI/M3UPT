1a272915d8a4dce1fe5c212655e4e8b3d02d38bc
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2023-05-29 02:39:38 +0100

    Update epg-portugal.sh

--- EPG/epg-portugal.sh
+++ EPG/epg-portugal.sh
@@ -2,7 +2,7 @@
 
 cd EPG
 
-tv_sort --by-channel --output epg-vodafone-pt-sorted.xml  epg-vodafone-pt.xml && tv_sort --by-channel --output epg-rtp-sorted.xml  epg-rtp.xml 
+tv_sort --by-channel --output epg-vodafone-pt-sorted.xml epg-vodafone-pt.xml && tv_sort --by-channel --output epg-rtp-sorted.xml epg-rtp.xml 
 
 tv_merge -i epg-vodafone-pt-sorted.xml -m epg-rtp-sorted.xml -o epg-portugal.xml && xz -k -f -9 epg-portugal.xml
 
