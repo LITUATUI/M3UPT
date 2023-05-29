7cf8779f45ab6b34840346deaf9c4f5c00b3e5f4
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2023-05-29 01:34:06 +0100

    Update epg-portugal.sh

--- EPG/epg-portugal.sh
+++ EPG/epg-portugal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-tv_merge -i epg-vodafone-pt.xml -m epg-rtp.xml -o epg-portugal.xml && xz -k -f -9 epg-portugal.xml
+tv_sort --by-channel --output epg-vodafone-pt-sorted.xml  epg-vodafone-pt.xml && tv_sort --by-channel --output epg-rtp-sorted.xml  epg-rtp.xml 
+
+tv_merge -i epg-vodafone-pt-sorted.xml -m epg-rtp-sorted.xml -o epg-portugal.xml && xz -k -f -9 epg-portugal.xml
 
 exit 0
