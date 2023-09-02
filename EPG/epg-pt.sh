4d3bd0ea4363e1098230c43d18fde4bcdfc3656f
seabrus <g.seabra@protonmail.com>
2023-09-02 11:11:10 +0100

    feat: add gz outputs for Vodafone and MEO EPGs

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -8,11 +8,11 @@ wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
 
 # Vodafone EPG
 
-wget -N "https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone" && perl tv_grab_pt_vodafone --config-file vodafone-pt.conf --output epg-vodafone-pt.xml && xz -k -f -9 epg-vodafone-pt.xml
+wget -N "https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone" && perl tv_grab_pt_vodafone --config-file vodafone-pt.conf --output epg-vodafone-pt.xml && xz -k -f -9 epg-vodafone-pt.xml && gzip -k -f -9 epg-vodafone-pt.xml
 
 # Meo EPG
 
-cd ../iptv-org-epg && npm install && npx epg-grabber --config=sites/meo.pt/meo.pt.config.js --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 && cd ../EPG && xz -k -f -9 epg-meo-pt.xml
+cd ../iptv-org-epg && npm install && npx epg-grabber --config=sites/meo.pt/meo.pt.config.js --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 && cd ../EPG && xz -k -f -9 epg-meo-pt.xml && gzip -k -f -9 epg-meo-pt.xml
 
 # Remove xml files
 
