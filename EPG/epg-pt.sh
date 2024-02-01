a47ca629b1547af64d9643ab8cdc308321b190a5
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2024-02-01 10:12:04 +0000

    Update EPG scripts

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -8,21 +8,25 @@ wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
 
 # Vodafone EPG
 
-wget -N "https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone" && perl tv_grab_pt_vodafone --config-file vodafone-pt.conf --output epg-vodafone-pt.xml && xz -k -f -9 epg-vodafone-pt.xml && gzip -k -f -9 epg-vodafone-pt.xml
+wget -N "https://github.com/XMLTV/xmltv/raw/master/grab/pt_vodafone/tv_grab_pt_vodafone" && perl tv_grab_pt_vodafone --config-file vodafone-pt.conf --output epg-vodafone-pt.xml
 
 # Meo EPG
 
-cd ../iptv-org-epg && npm install && npx epg-grabber --config=sites/meo.pt/meo.pt.config.js --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 && cd ../EPG && xz -k -f -9 epg-meo-pt.xml && gzip -k -f -9 epg-meo-pt.xml
+cd ../iptv-org-epg && npm run grab -- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7
 
 # Nos EPG
 
-cd ../iptv-org-epg && npm install && npx epg-grabber --config=sites/nos.pt/nos.pt.config.js --channels=../EPG/nos.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 && cd ../EPG && xz -k -f -9 epg-nos-pt.xml && gzip -k -f -9 epg-nos-pt.xml
+cd ../iptv-org-epg && npm run grab -- --site=nostv.pt --output=../EPG/epg-nos-pt.xml --days=7
 
 # RTP EPG
 
-cd ../iptv-org-epg && npm install && npx epg-grabber --config=sites/rtp.pt/rtp.pt.config.js --channels=../EPG/rtp.pt.channels.xml --output=../EPG/epg-rtp-pt.xml --days=7 && cd ../EPG && xz -k -f -9 epg-rtp-pt.xml && gzip -k -f -9 epg-rtp-pt.xml
+cd ../iptv-org-epg && npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7
 
-# Remove xml files
+# Compress EPG xml files
+
+cd ../EPG && xz -k -f -9 *.xml && gzip -k -f -9 *.xml
+
+# Remove EPG xml files
 
 rm *.xml
 
