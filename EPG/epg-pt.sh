0fe6999912be319a39f9b3454ff9bb8699c2325a
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2024-03-12 09:25:43 +0000

    Update epg-pt.sh

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -4,15 +4,15 @@ cd /home/runner/work/M3UPT/M3UPT/iptv-org-epg && npm install
 
 # Meo EPG
 
-npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=10
+npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=5
 
 # Nos EPG
 
-npm run grab -- --channels=../EPG/nostv.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=10
+npm run grab -- --channels=../EPG/nostv.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=5
 
 # RTP EPG
 
-npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=10
+npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=5
 
 # Rytec EPG
 
