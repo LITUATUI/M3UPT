841553101e66ec27575b6918479f9d6b0306d9dc
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2024-05-07 22:53:07 +0100

    Updates

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -4,11 +4,11 @@ cd /home/runner/work/M3UPT/M3UPT/iptv-org-epg && npm install
 
 # Meo EPG
 
-npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=2
+npm run grab -- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=2
 
 # Nos EPG
 
-npm run grab -- --channels=../EPG/nostv.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=10
+npm run grab -- --site=nostv.pt --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=10
 
 # RTP EPG
 
