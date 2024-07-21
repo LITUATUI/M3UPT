d11845a3ef81184b0b48c8a4fe8465c57d9c82f8
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2024-07-21 22:59:49 +0100

    Fix #83

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -13,9 +13,3 @@ sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?
 # TVI Internacional - update the stream URL of TVI Internacional
 
 sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
-
-# Porto Canal - update the stream URL of Porto Canal
-
-python porto_canal.py && sed -e '/x8egnb8/ {' -e 'r porto_canal.txt' -e 'd' -e '}' -i M3UPT.m3u
-
-exit 0
