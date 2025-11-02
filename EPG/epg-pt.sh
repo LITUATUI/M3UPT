bd00bd9353fc71dd68c728452f0b04aec9983dbf
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2025-11-02 15:12:16 +0000

    Update epg-pt.sh

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -4,11 +4,11 @@ cd /home/runner/work/M3UPT/M3UPT/iptv-org-epg && npm install
 
 # PT EPG
 
-npm run grab --- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=20
+npm run grab --- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=10
 
 # Meo EPG
 
-npm run grab --- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=5
+npm run grab --- --site=meo.pt --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=2
 
 # Nos EPG
 
@@ -20,7 +20,7 @@ npm run grab --- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConn
 
 # Mi.tv EPG
 
-npm run grab --- --channels=sites/mi.tv/mi.tv_br.channels.xml --output=../EPG/epg-mitv-br.xml --days=7 --maxConnections=10
+npm run grab --- --channels=sites/mi.tv/mi.tv_br.channels.xml --output=../EPG/epg-mitv-br.xml --days=7 --maxConnections=2
 
 # Rytec EPG
 
