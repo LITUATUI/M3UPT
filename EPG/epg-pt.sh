aee841935e98ab297bc0d454ea22e71746b639c9
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2026-01-01 01:41:53 +0000

    Updates

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -80,7 +80,7 @@ cd ../EPG && wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
 
 # Compress EPG xml files
 
-xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg-m3upt.xml
 
 # Remove EPG xml files
 
