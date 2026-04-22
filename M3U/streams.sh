064da23418304eabf866678d142f4c56db4c48c1
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2026-04-22 23:33:58 +0100

    Updates

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -37,6 +37,18 @@ sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matr
 
 sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "CNN_Portugal.m3u"
 
+# V+ TVI - update the stream URL of V+ TVI
+
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "Vmais_TVI.m3u"
+
+# TVI Ficção - update the stream URL of TVI Ficção
+
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Ficcao.m3u"
+
+# TVI Reality - update the stream URL of TVI Reality
+
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Reality.m3u"
+
 # TVI Internacional - update the stream URL of TVI Internacional
 
 sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Internacional.m3u"
