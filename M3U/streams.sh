63c2cd0b4279ead0a580d43d48fc96048d899487
Pedro Lima <49299328+LITUATUI@users.noreply.github.com>
2026-04-22 15:44:00 +0100

    Fix TVI channels

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -10,6 +10,18 @@ sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/play
 
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
 
+# V+ TVI - update the stream URL of V+ TVI
+
+sed -i "/live_vmais/ c https://video-auth2.iol.pt/live_vmais/live_vmais/edge_servers/vmais-720p/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# TVI Ficção - update the stream URL of TVI Ficção
+
+sed -i "/live_tvi_ficcao/ c https://video-auth1.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# TVI Reality - update the stream URL of TVI Reality
+
+sed -i "/live_tvi_reality/ c https://video-auth4.iol.pt/live_tvi_reality/live_tvi_reality/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
 # TVI Internacional - update the stream URL of TVI Internacional
 
 sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
@@ -20,12 +32,12 @@ sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internaci
 
 sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI.m3u"
 
-# TVI Internacional - update the stream URL of TVI Internacional
-
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Internacional.m3u"
-
 # CNN Portugal - update the stream URL of CNN Portugal
 
 sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "CNN_Portugal.m3u"
+
+# TVI Internacional - update the stream URL of TVI Internacional
+
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI_Internacional.m3u"
 
 exit 0
